309ec1b56efa5998f683bcdd4f67c155ac4dc4d1
yhr562605133 <562605133@qq.com>
2019-03-27 15:17:57 +0800

    杨浩然为了简化jsonrpc配置 对jsonrpc 1.5.3做出以下更改:   1.修改了以全类名为映射路径 @JsonRpcService不用再定义路径   2.对jsonrpc原先定义path时候必须最后加"\"，修改为可以不用加或者加都可以

--- src/main/java/com/googlecode/jsonrpc4j/spring/AutoJsonRpcServiceImplExporter.java
+++ src/main/java/com/googlecode/jsonrpc4j/spring/AutoJsonRpcServiceImplExporter.java
@@ -83,7 +83,7 @@ public class AutoJsonRpcServiceImplExporter implements BeanFactoryPostProcessor 
 				
 				List<String> paths = new ArrayList<>();
 				Collections.addAll(paths, autoJsonRpcServiceImplAnnotation.additionalPaths());
-				paths.add(jsonRpcServiceAnnotation.value());
+				paths.add(getRpcInterfaceName((DefaultListableBeanFactory) beanFactory,beanName));
 				
 				for (String path : paths) {
 					if (!PATTERN_JSONRPC_PATH.matcher(path).matches()) {
@@ -127,7 +127,19 @@ public class AutoJsonRpcServiceImplExporter implements BeanFactoryPostProcessor 
 	private static boolean hasServiceAnnotation(JsonRpcService jsonRpcPath) {
 		return jsonRpcPath != null;
 	}
-	
+
+	private static String  getRpcInterfaceName(DefaultListableBeanFactory defaultListableBeanFactory, String serviceBeanName) {
+		BeanDefinition serviceBeanDefinition = findBeanDefinition(defaultListableBeanFactory, serviceBeanName);
+		for (Class<?> currentInterface : getBeanInterfaces(serviceBeanDefinition, defaultListableBeanFactory.getBeanClassLoader())) {
+			if (currentInterface.isAnnotationPresent(JsonRpcService.class)) {
+				return currentInterface.getName();
+
+			}
+		}
+		return null;
+	}
+
+
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
 		DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
 		Map<String, String> servicePathToBeanName = findServiceBeanDefinitions(defaultListableBeanFactory);
@@ -211,7 +223,7 @@ public class AutoJsonRpcServiceImplExporter implements BeanFactoryPostProcessor 
 	/**
 	 * Find a {@link BeanDefinition} in the {@link BeanFactory} or it's parents.
 	 */
-	private BeanDefinition findBeanDefinition(ConfigurableListableBeanFactory beanFactory, String serviceBeanName) {
+	private static  BeanDefinition findBeanDefinition(ConfigurableListableBeanFactory beanFactory, String serviceBeanName) {
 		if (beanFactory.containsLocalBean(serviceBeanName)) {
 			return beanFactory.getBeanDefinition(serviceBeanName);
 		}
@@ -222,7 +234,7 @@ public class AutoJsonRpcServiceImplExporter implements BeanFactoryPostProcessor 
 		throw new NoSuchBeanDefinitionException(serviceBeanName);
 	}
 	
-	private Class<?>[] getBeanInterfaces(BeanDefinition serviceBeanDefinition, ClassLoader beanClassLoader) {
+	private static  Class<?>[] getBeanInterfaces(BeanDefinition serviceBeanDefinition, ClassLoader beanClassLoader) {
 		String beanClassName = serviceBeanDefinition.getBeanClassName();
 		try {
 			Class<?> beanClass = forName(beanClassName, beanClassLoader);
